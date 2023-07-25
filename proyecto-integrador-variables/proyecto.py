073592d7ab234ed796a642055576f73b33f4244f
alejandro <alejosamp@gmail.com>
2023-07-25 10:24:14 -0500

    proyecto integrador variables

---
+++ proyecto-integrador-variables/proyecto.py
@@ -0,0 +1,9 @@
+def main():
+    # Pedir el nombre del jugador por teclado
+    nombre_jugador = input("Ingrese su nombre: ")
+
+    # Imprimir un mensaje de bienvenida con el nombre
+    print(f"Bienvenido al programa, {nombre_jugador}!")
+
+if __name__ == "__main__":
+    main()
